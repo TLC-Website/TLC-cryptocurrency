411fe596fdc49f9d3532a1b332b395a4112b3c7a
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2025-10-23 16:13:40 +0200

    Implement signup and signin functionality: update forms in index.html, add views and templates for signup and signin

--- TLCapp/views.py
+++ TLCapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {"title": "Welcome", "user": "Alice"}
-    return render(request,"index.html", context)
+    return render(request,"index.html")
+
+def singup(request):
+    return render(request,"singup.html")
+
+def singin(request):
+    return render(request,"singin.html")
