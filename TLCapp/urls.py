c11018753017041347d04842c53d1c775f92810a
vlzakh689 <zakharov.vlad2009@gmail.com>
2025-11-02 16:56:48 +0100

    Refactor project structure: add main page, update templates, implement user registration, and remove unused files

--- TLCapp/urls.py
+++ TLCapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("singup", views.singup, name="singup"),
     path("singin", views.singin, name="singin"),
+    path("main", views.main, name="main"),
 ]
--- TLCapp/views.py
+++ TLCapp/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+from .forms import RegisterForm
+from django.contrib import messages
 
 def home(request):
     return render(request,"home.html")
 
 def singup(request):
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            login(request, user)
+            return redirect("/main")
+        
+        else:
+            messages.error(request, "Registration failed. Please correct the errors below.")
+
+    else:
+        form = RegisterForm()
+
     return render(request,"singup.html")
+
+
+
 
 def singin(request):
     return render(request,"singin.html")
+
+def main(request):
+    return render(request,"main.html")
