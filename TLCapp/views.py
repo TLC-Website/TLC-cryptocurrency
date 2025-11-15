a8b70bb45f6cb0fb3e0c2b4a92add2915e777d1e
Bohdan.Klymov <programmer.b.k.339@gmail.com>
2025-11-15 15:50:44 +0100

    Implemented a video playback feature that plays an introductory video.
    Login feature implemented.

--- TLCapp/views.py
+++ TLCapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
-from .forms import RegisterForm
-from django.contrib import messages
+from django.contrib.auth import login
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request,"home.html")
@@ -26,7 +26,6 @@ def singup(request):
             print("User registered:", user)
 
             return redirect("/main")
-            
 
         else:
             return render(request, "singup.html", {"form": form})
@@ -37,8 +36,23 @@ def singup(request):
 
     return render(request,"singup.html", {"form": form})
 
-def singin(request):
-    return render(request,"singin.html")
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("/main")
+    else:
+        form = LoginForm()
+
+    return render(request, "singin.html", {"form": form})
 
 def main(request):
     return render(request,"main.html")
